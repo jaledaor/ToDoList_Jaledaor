f207bd2253de1cd70ea4bd21172e69795cdc0c75
jaledaor <hueckham@gmail.com>
2017-09-12 20:19:47 -0500

    v0.0.3

--- app/src/main/java/co/edu/ucc/todolist_jaledaor/dominio/LTarea.java
+++ app/src/main/java/co/edu/ucc/todolist_jaledaor/dominio/LTarea.java
@@ -9,15 +9,16 @@ import co.edu.ucc.todolist_jaledaor.modelo.Tarea;
  * Created by ADMIN on 05/09/2017.
  */
 
-public class LTarea {
+public class LTarea implements ILtarea {
 
     private static List<Tarea> lstTarea= new ArrayList<>();
 
+    @Override
     public void addTarea(Tarea tarea) {
-
     lstTarea.add(tarea);
     }
 
+    @Override
     public List<Tarea> getTareas(){
         return lstTarea;
     }
--- app/src/main/java/co/edu/ucc/todolist_jaledaor/vistas/ListActivity.java
+++ app/src/main/java/co/edu/ucc/todolist_jaledaor/vistas/ListActivity.java
@@ -3,13 +3,32 @@ package co.edu.ucc.todolist_jaledaor.vistas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import co.edu.ucc.todolist_jaledaor.R;
 
-public class ListActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity implements IListView{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnEnviarTarea)
+    @Override
+    public void clickAddtarea() {
+
+    }
+
+    @Override
+    public void mostrarListaTareas() {
+
+    }
+
+    @Override
+    public void actualizarItem(int posicion, boolean estado) {
+
     }
 }
