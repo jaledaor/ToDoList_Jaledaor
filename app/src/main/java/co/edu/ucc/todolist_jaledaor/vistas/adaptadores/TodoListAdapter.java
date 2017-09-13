46530a705877fb46a2a8f964c31bd45bf7064f96
jaledaor <hueckham@gmail.com>
2017-09-12 20:36:18 -0500

    v0.0.5

--- app/src/main/java/co/edu/ucc/todolist_jaledaor/vistas/adaptadores/TodoListAdapter.java
+++ app/src/main/java/co/edu/ucc/todolist_jaledaor/vistas/adaptadores/TodoListAdapter.java
@@ -3,6 +3,7 @@ package co.edu.ucc.todolist_jaledaor.vistas.adaptadores;
 import android.support.v7.widget.AppCompatCheckBox;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.textservice.TextInfo;
 import android.widget.TextView;
 
@@ -14,7 +15,22 @@ import co.edu.ucc.todolist_jaledaor.R;
  * Created by ADMIN on 12/09/2017.
  */
 
-public class TodoListAdapter {
+public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.ItemTodoList>{
+
+    @Override
+    public ItemTodoList onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ItemTodoList holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class ItemTodoList extends RecyclerView.ViewHolder{
 
